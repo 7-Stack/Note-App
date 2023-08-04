885f4a5ae8d9885afee597b6aec26b24ca6707ed
7-Stack <ashleyculture@yahoo.com>
2023-08-04 15:23:56 +0100

    Second Commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/notes", require("./routes/noteRoutes"));
+app.use(`/api/notes`, require("./routes/noteRoutes"));
+app.use(`/api/users`, require("./routes/userRoutes"));
+
+app.get("/", (req,res) => {
+    res.send()
+})
+app.get(`/api/v1/docs`, (req,res) => {
+    res.redirect("http://localhost:5009/api/notes")
+})
 app.use(errorHandler);
 
 module.exports = app;
