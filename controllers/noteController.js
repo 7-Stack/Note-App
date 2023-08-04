e45ee622550e2f0f9975312b6df1e4bfff604823
7-Stack <ashleyculture@yahoo.com>
2023-08-04 15:16:08 +0100

    Route Logic

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -2,15 +2,15 @@ const express = require("express");
 const Note = require("../models/noteModel");
 //@desc Get all  notes
 //@route POST /api/notes
-//@access public
+//@access private
 const getNotes = async(req, res) => {
-    const notes = await Note.find();
+    const notes = await Note.find({ user_id: req.user.id });
     res.status(200).json(notes);
 };
 
 //@desc Create new note
 //@route POST /api/notes
-//@access public
+//@access private
 const createNote = async(req, res) => {
     console.log("The request body is :", req.body);
     const { name, author, numberOfPages  } = req.body;
@@ -22,13 +22,14 @@ const createNote = async(req, res) => {
         name,
         author,
         numberOfPages,
+        user_id: req.user.id,
     });
     res.status(201).json(note);
 };
 
 //@desc Get note
 //@route POST /api/notes/:id
-//@access public
+//@access private
 const getNote = async(req, res) => {
     const note = await Note.findById(req.params.id);
     if(!note) {
@@ -40,12 +41,17 @@ const getNote = async(req, res) => {
 
 //@desc Update note
 //@route PUT /api/notes/:id
-//@access public
+//@access private
 const updateNote = async(req, res) => {
     const note = await Note.findById(req.params.id);
     if(!note) {
         res.status(404);
         throw new Error ("Note not found");
+    }
+
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User does not have permission to update other user notes");
     }
     const updatedNote = await Note.findByIdAndUpdate(
         req.params.id,
@@ -57,14 +63,18 @@ const updateNote = async(req, res) => {
 
 //@desc Delete note
 //@route DELETE /api/notes/:id
-//@access public
+//@access private
 const deleteNote = async(req, res) => {
     const note = await Note.findById(req.params.id);
     if(!note) {
         res.status(404);
         throw new Error ("Note not found");
        }
-       await note.remove();
+       if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User does not have permission to delete other user notes");
+    }
+       await note.deleteOne({ _id: req.params.id});
     res.status(200).json(note);
 };
 
